85dc299c8bf54c83e94e14b62a2c2ed25f7f65b6
Gilberto de Oliveira <gilbertouba@yahoo.com.br>
2021-01-08 17:46:49 -0300

    Inclusão, Alteração e Deleção

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,9 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -20,20 +25,103 @@ public class SellerDaoJDBC implements SellerDao {
 	
 	@Override
 	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
+	
+		PreparedStatement st = null;
+		ResultSet 		  rs = null;		
 		
+		try {
+			
+			String sql;
+			
+			sql = " insert into seller (name,email,birthdate,basesalary,departmentid) ";
+			sql = sql + " values (?,?,?,?,?) ";
+			
+			st = conn.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1,obj.getName());
+			st.setString(2,obj.getEmai());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4,obj.getBaseSalary() );
+			st.setInt(5, obj.getDepartamento().getId());
+			
+			int rowAffected = st.executeUpdate();
+			
+			if (rowAffected>0) {
+				
+				rs = st.getGeneratedKeys();
+				if (rs.next()){
+					int id = rs.getInt(1);
+					obj.setId(id);				
+				}				
+			}
+			else {
+			
+				throw new DbException("Erro Inesperado");
+				
+			}				
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);			
+		}
 	}
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		
+		try {
+			
+			String sql;
+			
+			sql = " update seller set name = ? ,email = ?,birthdate = ?,basesalary = ?,departmentid=? ";
+			sql = sql + " where id=? ";
+			
+			st = conn.prepareStatement(sql);
+			
+			st.setString(1,obj.getName());
+			st.setString(2,obj.getEmai());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4,obj.getBaseSalary() );
+			st.setInt(5, obj.getDepartamento().getId());
+			st.setInt(6, obj.getId());			
+			st.executeUpdate();			
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			
+			String sql;
+			
+			sql = " delete from seller where id=? ";
+			
+			st = conn.prepareStatement(sql);
+			
+			st.setInt(1,id);			
+			st.executeUpdate();			
+			
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+		}	
 	}
 
 	@Override
@@ -88,8 +176,68 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("select seller.*,department.name as DepName" 
+					+" from seller ,department where  seller.departmentid=department.id ");
+
+			rs=st.executeQuery();
+			List<Seller> list = new ArrayList<>();
+			Map<Integer,Department> map = new HashMap();
+			
+			while (rs.next()) {
+				
+				Department depto = map.get(rs.getInt("DepartmentId"));
+			
+				if (depto == null) {
+					depto = instatiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), depto);
+				}
+				
+				Seller obj     = instatiateSeller(rs,depto);
+				list.add(obj);
+			}
+			return list;					
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}	}
+
+	@Override
+	public List<Seller> FindByDepartment(Department dep) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("select seller.*,department.name as DepName" 
+					+" from seller ,department where  seller.departmentid=department.id and"
+					+" department.id=? order by department.name ");
+			
+			st.setInt(1,dep.getId());
+			rs=st.executeQuery();
+			Department depto;
+			List<Seller> list = new ArrayList<>();
+			Map<Integer,Department> map = new HashMap();
+			while (rs.next()) {
+				depto = instatiateDepartment(rs);
+				Seller obj     = instatiateSeller(rs,depto);
+				list.add(obj);
+			}
+			return list;					
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+			return null;
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -1,7 +1,12 @@
 package teste;
 
+import java.util.Date;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Teste {
 
@@ -10,6 +15,25 @@ public class Teste {
 		SellerDao sellerDao = DaoFactory.cretaeSellerDao();
 		
 		System.out.println(sellerDao.findById(3));
+		//List<Seller> list = sellerDao.FindByDepartment(new Department(2,null));
+		List<Seller> list = sellerDao.findAll();
+		for (Seller obj:list) {
+			System.out.println(obj);
+			System.out.println(obj.getDepartamento().hashCode());
+		}
+		
+		/*Seller newSeller = new Seller(null,"Gilbero","gilbertouba@gmail.com",new Date(),4000.00,new Department(2,null));
+		
+		sellerDao.insert(newSeller);*/
+		
+		Seller sellerupd = sellerDao.findById(8);
+		if (sellerupd!=null) {
+			sellerupd.setName("Gilberto de Oliveira");
+			sellerDao.update(sellerupd);
+			System.out.println("alterado");			
+		}
+		sellerDao.deleteById(9);
+		
 	}
 
 }
